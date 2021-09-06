17e7a64ff3dddbb7b8f820f839534bade160bd6b
deoagent <deoagent@gmail.com>
2021-09-06 14:54:52 +0800

    #Model & Controller Settings

---
+++ app/Models/Student.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $table = 'students';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone_no'
+    ];
+
+    public $timestamps = false;
+}
